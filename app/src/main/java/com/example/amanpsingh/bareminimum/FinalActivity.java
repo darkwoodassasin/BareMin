db2f46ea6bf16b359e9c81122e1398313ef836f2
darkwoodassasin <2013darkwoodassasin@users.noreply.github.com>
2017-08-14 18:18:24 +0530

    new commit

--- app/src/main/java/com/example/amanpsingh/bareminimum/FinalActivity.java
+++ app/src/main/java/com/example/amanpsingh/bareminimum/FinalActivity.java
@@ -15,7 +15,7 @@ public class FinalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_final);
 
         Intent intent = getIntent();
-        CharSequence text = intent.getCharSequenceExtra("Text");
+        CharSequence text = intent.getCharSequenceExtra("NewText");
 
         textView = (TextView) findViewById(R.id.text_view);
         textView.setText(text);
